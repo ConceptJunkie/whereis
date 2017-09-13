49b2a38a467e76ef970ba38deb8bd3b7e56f7c5a
Rick Gutleber <rickg@his.com>
2017-09-13 13:45:19 -0400

    adding whereis_cython.py to run the compiled library
    Oops!  I need to check this in as well.

---
+++ whereis_cython.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+from whereis import startUp
+
+#//******************************************************************************
+#//
+#//  __main__
+#//
+#//
+#//  Use this to run whereis once it's been compiled by Cython.
+#//
+#//******************************************************************************
+
+if __name__ == '__main__':
+    startUp( )
+
